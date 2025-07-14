b6874485e3cf23faa45d78f15bbfd1c11686c1ba
Sugam <pathaksugam99@gmail.com>
2025-07-14 15:59:52 +0545

    ts-learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// type user = {
+//   username : string;
+//   age : number;
+//   isLoggedIn : boolean
+// }
+
+import { useEffect, useState } from "react";
+
+// function App() {
+//   const users  : user = {
+//     username : "Sugam",
+//     age : 22,
+//     isLoggedIn : true
+//   }
+  
+//   return (
+//     <div>
+//       <h1>username :  {users.username}</h1>
+//       <p>age : {users.age}</p>
+//       <p>status : {users.isLoggedIn ? "online" : "Offline"}</p>
+    
+//     </div>
+//   );
+// }
+// export default App
+
+
+//--------------------------USE-STATE----------------------------------------------------
+
+// toggle ussing usestate and tsx
+
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+//   const handleClick = () => {
+//     setIsLoggedIn(!isLoggedIn)
+//   }
+
+
+//   return (
+//     <div>
+//       <p>status : {isLoggedIn ? "online" : "ka maryo !"}</p>
+//       <button onClick={handleClick}>toggle</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// ----------------------USE STATE FOR AN OBJECT-----------------------------------
+type user = {
+  name : string;
+  age : number;
+  isLoggedIn : boolean
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState <user>({
+    name : "daka",
+    age : 25,
+    isLoggedIn : false
+  })
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  useEffect(()=> {
+    document.title = `${user.name} is ${user.isLoggedIn ? "online" : "offline"}`
+  },[user.isLoggedIn])
+
+  const toggleLogin = () => {
+    setUser(prev => ({
+      ...prev,
+      isLoggedIn : !prev.isLoggedIn
+    }))
+  };
+
+  return(
+    <div>
+    <h1>username : {user.name}</h1>
+    <p>age : {user.age}</p>
+    <p>status : {user.isLoggedIn ? "online" : "offline"}</p>
+    <button onClick={toggleLogin}>toggle login</button>
+    </div>
   )
+  
 }
 
 export default App
